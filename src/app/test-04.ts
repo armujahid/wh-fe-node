2096b82103a995b42a560e18617e2287d0ae9538
Abdul Rauf <abdulraufmujahid@gmail.com>
2023-06-03 21:34:55 +0500

    angular 4

--- src/app/test-04.ts
+++ src/app/test-04.ts
@@ -6,24 +6,44 @@
 import { Component, NgModule  } from '@angular/core';
 import { RouterModule} from "@angular/router";
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
     selector : 'ng-app',
     template : `
                 <h2>Enter your first and last name</h2>
                 <div>
+                    <label for="firstName">First Name:</label>
+                    <input type="text" id="firstName" [(ngModel)]="firstName" (blur)="generateUsername()" />
+                </div>
+                <div>
+                    <label for="lastName">Last Name:</label>
+                    <input type="text" id="lastName" [(ngModel)]="lastName" (blur)="generateUsername()" />
+                </div>
+                <div *ngIf="username">
+                    <label for="username">Username:</label>
+                    <span id="username">{{ username }}</span>
                 </div>
                 `,
     styles : []
 })
 export class UserNameComponent {
-    
+    firstName: string = '';
+    lastName: string = '';
+    username: string = '';
 
+    generateUsername() {
+        if (this.firstName && this.lastName) {
+            const randomInt = Math.floor(Math.random() * 9) + 1;
+            this.username = `${this.firstName.toLowerCase()}_${this.lastName.toLowerCase()}_${randomInt}`;
+        }
+    }
 }
 
 @NgModule({
     imports : [
         CommonModule,
+        FormsModule,
         RouterModule.forChild([
             {
                 path : "",
