adcd1a3a2edf5a409b220aecd234368ce5f6e2ab
Abdul Rauf <abdulraufmujahid@gmail.com>
2023-06-03 22:14:10 +0500

    angular 1

--- src/app/test-01.ts
+++ src/app/test-01.ts
@@ -5,26 +5,36 @@
  * * Both [monthly_payment] and [late_payment] should print in the template in currency format : $1,234
  */
 
-import { Component, Input,NgModule  } from '@angular/core';
+import { Component, Input,NgModule } from '@angular/core';
 import { RouterModule } from "@angular/router";
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
     selector : 'ng-app',
     template : `<div>
                     <h2>Loan Details</h2>
-                    <b>Monthly Payment:</b> {{monthly_payment}} <br/>
-                    <b>Late Payment Fee : {{late_payment}}</b> <br/>
+                    <b>Loan Amount:</b> <input name="loanAmount" [(ngModel)]="loan_amount" type="number"/> <br/>
+                    <b>Monthly Payment:</b> {{monthly_payment !== 'N/A' ? (monthly_payment | currency) : monthly_payment}} <br/>
+                    <b>Late Payment Fee:</b> {{late_payment !== 'N/A' ? (late_payment | currency): late_payment}} <br/>
                 </div>`
 })
 export class Test01Component {
 
-    loan_amount:number = 1000;
-    monthly_payment:number = 200;
-    late_payment = 10;
+    @Input() loan_amount: number = 1000;
+
+    get monthly_payment() {
+        return this.loan_amount ? this.loan_amount * 0.02 : 'N/A';
+    }
+    get late_payment() {
+        return typeof this.monthly_payment === 'number' ? this.monthly_payment * 0.05 : 'N/A';
+    } 
 }
 
 @NgModule({
     imports : [
+        CommonModule,
+        FormsModule,
         RouterModule.forChild([
             {
                 path : "",
